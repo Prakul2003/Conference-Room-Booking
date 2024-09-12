4eca9ed64ab0d1c13becc8e18a945807ba6310c5
Prakul Khurana <Prakul2003@users.noreply.github.com>
2024-09-12 15:45:49 +0530

    db

--- connection.php
+++ connection.php
@@ -2,16 +2,16 @@
     $username = "andvjlrmxw";
     $servername = "conference-server.mysql.database.azure.com:3306";
     $psd = "MTRqQTdsj6RRcTp";
-    $dbname = "project_adp";
+    $dbname = "conference-database";
     
-//     $conn = mysqli_init();
-// mysqli_real_connect($conn, "conference-server.mysql.database.azure.com", "andvjlrmxw", "MTRqQTdsj6RRcTp", "project_adp", 3306, MYSQLI_CLIENT_SSL);
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, "conference-server.mysql.database.azure.com", "andvjlrmxw", "MTRqQTdsj6RRcTp", "conference-database", 3306, MYSQLI_CLIENT_SSL);
 
-    $conn = mysqli_connect($servername, $username, $psd, $dbname, 3306);
+    // $conn = mysqli_connect($servername, $username, $psd, $dbname, 3306);
 
-    if(!$conn){
-        die('Could not Connect MySql Server:'.mysqli_connect_error());
-    }
+    // if(!$conn){
+    //     die('Could not Connect MySql Server:'.mysqli_connect_error());
+    // }
     //else{
         //echo "Vatsal";
     //}
