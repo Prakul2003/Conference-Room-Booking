71791c4bbdded71ca7434dd4ca30ec9dcc18f9d4
Prakul Khurana <Prakul2003@users.noreply.github.com>
2024-09-12 14:42:51 +0530

    added db

--- connection.php
+++ connection.php
@@ -1,10 +1,13 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
+    $username = "andvjlrmxw";
+    $servername = "conference-server.mysql.database.azure.com";
     $psd = "";
     $dbname = "project_adp";
     
-    $conn = mysqli_connect($servername, $username, $psd, $dbname);
+    $con = mysqli_init();
+mysqli_real_connect($conn, "conference-server.mysql.database.azure.com", "andvjlrmxw", "MTRqQTdsj6RRcTp", "project_adp", 3306, MYSQLI_CLIENT_SSL);
+
+    // $conn = mysqli_connect($servername, $username, $psd, $dbname);
 
     if(!$conn){
         die('Could not Connect MySql Server:'.mysqli_connect_error());
