a56ba97b3a3a706c2cdcbf3eb53be32496a37941
Prakul Khurana <Prakul2003@users.noreply.github.com>
2024-09-12 14:52:07 +0530

    db fix

--- connection.php
+++ connection.php
@@ -1,13 +1,13 @@
 <?php
     $username = "andvjlrmxw";
     $servername = "conference-server.mysql.database.azure.com";
-    $psd = "";
+    $psd = "MTRqQTdsj6RRcTp";
     $dbname = "project_adp";
     
-    $con = mysqli_init();
-mysqli_real_connect($conn, "conference-server.mysql.database.azure.com", "andvjlrmxw", "MTRqQTdsj6RRcTp", "project_adp", 3306, MYSQLI_CLIENT_SSL);
+//     $conn = mysqli_init();
+// mysqli_real_connect($conn, "conference-server.mysql.database.azure.com", "andvjlrmxw", "MTRqQTdsj6RRcTp", "project_adp", 3306, MYSQLI_CLIENT_SSL);
 
-    // $conn = mysqli_connect($servername, $username, $psd, $dbname);
+    $conn = mysqli_connect($servername, $username, $psd, $dbname);
 
     if(!$conn){
         die('Could not Connect MySql Server:'.mysqli_connect_error());
